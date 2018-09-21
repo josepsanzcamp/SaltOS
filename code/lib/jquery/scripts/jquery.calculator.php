0ffb5b1cc2dd62711c80262529aade280e71ad35
Josep Sanz <Josep Sanz josep.sanz@saltos.org>
2018-09-21 23:45:43 +0200

    Updated to SaltOS r8692

--- code/lib/jquery/scripts/jquery.calculator.php
+++ code/lib/jquery/scripts/jquery.calculator.php
@@ -1,11 +1,11 @@
 <?php
 if($argc!=3) die("Unknown arguments!!!\n");
 $color=$argv[1];
-$div=$argv[2];
+$factor=$argv[2];
 $color=str_split($color,2);
 foreach($color as $key=>$val) {
 	$val=hexdec($val);
-	$val=$val/$div;
+	$val=$val*$factor;
 	$val=min($val,255);
 	$val=max($val,0);
 	$val=sprintf("%02x",$val);
--- code/lib/jquery/scripts/jquery.themeroller.sh
+++ code/lib/jquery/scripts/jquery.themeroller.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 
-rm -rf jquery-ui.custom.blue.light
-rm -rf jquery-ui.custom.green.light
-rm -rf jquery-ui.custom.purple.light
-rm -rf jquery-ui.custom.red.light
+rm -rf jquery-ui.custom.upc.light
+rm -rf jquery-ui.custom.uab.light
+rm -rf jquery-ui.custom.url.light
+rm -rf jquery-ui.custom.cat.light
 
-php scripts/jquery-ui.themeroller.php blue.light 2c579b ffffff ffffff 333333 f6f6f6 2c579b
-php scripts/jquery-ui.themeroller.php green.light 107c0f ffffff ffffff 333333 f6f6f6 107c0f
-php scripts/jquery-ui.themeroller.php purple.light 80397b ffffff ffffff 333333 f6f6f6 80397b
-php scripts/jquery-ui.themeroller.php red.light d24726 ffffff ffffff 333333 f6f6f6 d24726
+php scripts/jquery-ui.themeroller.php upc.light 007BC1 ffffff ffffff 333333 f6f6f6 003d60
+php scripts/jquery-ui.themeroller.php uab.light 00856c ffffff ffffff 333333 f6f6f6 004236
+php scripts/jquery-ui.themeroller.php url.light 990002 ffffff ffffff 333333 f6f6f6 4c0001
+php scripts/jquery-ui.themeroller.php cat.light ffc500 000000 ffffff 333333 f6f6f6 7f6200
 
-rm -rf jquery-ui.custom.blue.dark
-rm -rf jquery-ui.custom.green.dark
-rm -rf jquery-ui.custom.purple.dark
-rm -rf jquery-ui.custom.red.dark
+rm -rf jquery-ui.custom.upc.dark
+rm -rf jquery-ui.custom.uab.dark
+rm -rf jquery-ui.custom.url.dark
+rm -rf jquery-ui.custom.cat.dark
 
-php scripts/jquery-ui.themeroller.php blue.dark 2c579b ffffff 000000 ffffff 333333 f6f6f6
-php scripts/jquery-ui.themeroller.php green.dark 107c0f ffffff 000000 ffffff 333333 f6f6f6
-php scripts/jquery-ui.themeroller.php purple.dark 80397b ffffff 000000 ffffff 333333 f6f6f6
-php scripts/jquery-ui.themeroller.php red.dark d24726 ffffff 000000 ffffff 333333 f6f6f6
+php scripts/jquery-ui.themeroller.php upc.dark 007BC1 ffffff 000000 ffffff 001e30 f6f6f6
+php scripts/jquery-ui.themeroller.php uab.dark 00856c ffffff 000000 ffffff 00211b f6f6f6
+php scripts/jquery-ui.themeroller.php url.dark 990002 ffffff 000000 ffffff 260000 f6f6f6
+php scripts/jquery-ui.themeroller.php cat.dark ffc500 000000 000000 ffffff 3f3100 f6f6f6
 
-rm -rf jquery.mobile.custom.blue.light
-rm -rf jquery.mobile.custom.green.light
-rm -rf jquery.mobile.custom.purple.light
-rm -rf jquery.mobile.custom.red.light
+rm -rf jquery.mobile.custom.upc.light
+rm -rf jquery.mobile.custom.uab.light
+rm -rf jquery.mobile.custom.url.light
+rm -rf jquery.mobile.custom.cat.light
 
-php scripts/jquery.mobile.themeroller.php blue.light 2c579b ffffff ffffff 333333 f6f6f6 2c579b
-php scripts/jquery.mobile.themeroller.php green.light 107c0f ffffff ffffff 333333 f6f6f6 107c0f
-php scripts/jquery.mobile.themeroller.php purple.light 80397b ffffff ffffff 333333 f6f6f6 80397b
-php scripts/jquery.mobile.themeroller.php red.light d24726 ffffff ffffff 333333 f6f6f6 d24726
+php scripts/jquery.mobile.themeroller.php upc.light 007BC1 ffffff ffffff 333333 f6f6f6 003d60
+php scripts/jquery.mobile.themeroller.php uab.light 00856c ffffff ffffff 333333 f6f6f6 004236
+php scripts/jquery.mobile.themeroller.php url.light 990002 ffffff ffffff 333333 f6f6f6 4c0001
+php scripts/jquery.mobile.themeroller.php cat.light ffc500 000000 ffffff 333333 f6f6f6 7f6200
 
-rm -rf jquery.mobile.custom.blue.dark
-rm -rf jquery.mobile.custom.green.dark
-rm -rf jquery.mobile.custom.purple.dark
-rm -rf jquery.mobile.custom.red.dark
+rm -rf jquery.mobile.custom.upc.dark
+rm -rf jquery.mobile.custom.uab.dark
+rm -rf jquery.mobile.custom.url.dark
+rm -rf jquery.mobile.custom.cat.dark
 
-php scripts/jquery.mobile.themeroller.php blue.dark 2c579b ffffff 000000 ffffff 333333 f6f6f6
-php scripts/jquery.mobile.themeroller.php green.dark 107c0f ffffff 000000 ffffff 333333 f6f6f6
-php scripts/jquery.mobile.themeroller.php purple.dark 80397b ffffff 000000 ffffff 333333 f6f6f6
-php scripts/jquery.mobile.themeroller.php red.dark d24726 ffffff 000000 ffffff 333333 f6f6f6
+php scripts/jquery.mobile.themeroller.php upc.dark 007BC1 ffffff 000000 ffffff 001e30 f6f6f6
+php scripts/jquery.mobile.themeroller.php uab.dark 00856c ffffff 000000 ffffff 00211b f6f6f6
+php scripts/jquery.mobile.themeroller.php url.dark 990002 ffffff 000000 ffffff 260000 f6f6f6
+php scripts/jquery.mobile.themeroller.php cat.dark ffc500 000000 000000 ffffff 3f3100 f6f6f6
