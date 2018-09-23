5a5d2bf367faf8767606721c78a310b7dd237e30
Josep Sanz <Josep Sanz josep.sanz@saltos.org>
2018-09-23 23:45:40 +0200

    Updated to SaltOS r8697

--- code/lib/jquery/scripts/jquery.themeroller.sh
+++ code/lib/jquery/scripts/jquery.themeroller.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 
-rm -rf jquery-ui.custom.upc.light
-rm -rf jquery-ui.custom.uab.light
-rm -rf jquery-ui.custom.url.light
-rm -rf jquery-ui.custom.cat.light
+rm -rf jquery-ui.custom.blue.light
+rm -rf jquery-ui.custom.green.light
+rm -rf jquery-ui.custom.red.light
+rm -rf jquery-ui.custom.yellow.light
 
-php scripts/jquery-ui.themeroller.php upc.light 007BC1 ffffff ffffff 333333 f6f6f6 003d60
-php scripts/jquery-ui.themeroller.php uab.light 00856c ffffff ffffff 333333 f6f6f6 004236
-php scripts/jquery-ui.themeroller.php url.light 990002 ffffff ffffff 333333 f6f6f6 4c0001
-php scripts/jquery-ui.themeroller.php cat.light ffc500 000000 ffffff 333333 f6f6f6 7f6200
+php scripts/jquery-ui.themeroller.php blue.light 4285f4 ffffff ffffff 000000 f0f6ff 1a3561
+php scripts/jquery-ui.themeroller.php green.light 34a853 ffffff ffffff 000000 f0fff8 144321
+php scripts/jquery-ui.themeroller.php red.light ea4335 ffffff ffffff 000000 fff0f0 5d1a15
+php scripts/jquery-ui.themeroller.php yellow.light fbbc05 000000 ffffff 000000 fffff0 644b02
 
-rm -rf jquery-ui.custom.upc.dark
-rm -rf jquery-ui.custom.uab.dark
-rm -rf jquery-ui.custom.url.dark
-rm -rf jquery-ui.custom.cat.dark
+rm -rf jquery-ui.custom.blue.dark
+rm -rf jquery-ui.custom.green.dark
+rm -rf jquery-ui.custom.red.dark
+rm -rf jquery-ui.custom.yellow.dark
 
-php scripts/jquery-ui.themeroller.php upc.dark 007BC1 ffffff 000000 ffffff 001e30 f6f6f6
-php scripts/jquery-ui.themeroller.php uab.dark 00856c ffffff 000000 ffffff 00211b f6f6f6
-php scripts/jquery-ui.themeroller.php url.dark 990002 ffffff 000000 ffffff 260000 f6f6f6
-php scripts/jquery-ui.themeroller.php cat.dark ffc500 000000 000000 ffffff 3f3100 f6f6f6
+php scripts/jquery-ui.themeroller.php blue.dark 4285f4 ffffff 000000 ffffff 0d1a30 ffffff
+php scripts/jquery-ui.themeroller.php green.dark 34a853 ffffff 000000 ffffff 0a2110 ffffff
+php scripts/jquery-ui.themeroller.php red.dark ea4335 ffffff 000000 ffffff 2e0d0a ffffff
+php scripts/jquery-ui.themeroller.php yellow.dark fbbc05 000000 000000 ffffff 322501 ffffff
 
-rm -rf jquery.mobile.custom.upc.light
-rm -rf jquery.mobile.custom.uab.light
-rm -rf jquery.mobile.custom.url.light
-rm -rf jquery.mobile.custom.cat.light
+rm -rf jquery.mobile.custom.blue.light
+rm -rf jquery.mobile.custom.green.light
+rm -rf jquery.mobile.custom.red.light
+rm -rf jquery.mobile.custom.yellow.light
 
-php scripts/jquery.mobile.themeroller.php upc.light 007BC1 ffffff ffffff 333333 f6f6f6 003d60
-php scripts/jquery.mobile.themeroller.php uab.light 00856c ffffff ffffff 333333 f6f6f6 004236
-php scripts/jquery.mobile.themeroller.php url.light 990002 ffffff ffffff 333333 f6f6f6 4c0001
-php scripts/jquery.mobile.themeroller.php cat.light ffc500 000000 ffffff 333333 f6f6f6 7f6200
+php scripts/jquery.mobile.themeroller.php blue.light 4285f4 ffffff ffffff 000000 f0f6ff 1a3561
+php scripts/jquery.mobile.themeroller.php green.light 34a853 ffffff ffffff 000000 f0fff8 144321
+php scripts/jquery.mobile.themeroller.php red.light ea4335 ffffff ffffff 000000 fff0f0 5d1a15
+php scripts/jquery.mobile.themeroller.php yellow.light fbbc05 000000 ffffff 000000 fffff0 644b02
 
-rm -rf jquery.mobile.custom.upc.dark
-rm -rf jquery.mobile.custom.uab.dark
-rm -rf jquery.mobile.custom.url.dark
-rm -rf jquery.mobile.custom.cat.dark
+rm -rf jquery.mobile.custom.blue.dark
+rm -rf jquery.mobile.custom.green.dark
+rm -rf jquery.mobile.custom.red.dark
+rm -rf jquery.mobile.custom.yellow.dark
 
-php scripts/jquery.mobile.themeroller.php upc.dark 007BC1 ffffff 000000 ffffff 001e30 f6f6f6
-php scripts/jquery.mobile.themeroller.php uab.dark 00856c ffffff 000000 ffffff 00211b f6f6f6
-php scripts/jquery.mobile.themeroller.php url.dark 990002 ffffff 000000 ffffff 260000 f6f6f6
-php scripts/jquery.mobile.themeroller.php cat.dark ffc500 000000 000000 ffffff 3f3100 f6f6f6
+php scripts/jquery.mobile.themeroller.php blue.dark 4285f4 ffffff 000000 ffffff 0d1a30 ffffff
+php scripts/jquery.mobile.themeroller.php green.dark 34a853 ffffff 000000 ffffff 0a2110 ffffff
+php scripts/jquery.mobile.themeroller.php red.dark ea4335 ffffff 000000 ffffff 2e0d0a ffffff
+php scripts/jquery.mobile.themeroller.php yellow.dark fbbc05 000000 000000 ffffff 322501 ffffff
