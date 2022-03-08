7175a303e657d13f399fe05e4c1677e3e5e0b07f
Josep Sanz <Josep Sanz josep.sanz@saltos.org>
2022-03-08 23:45:25 +0100

    Updated to SaltOS r9768

--- code/php/action/captcha.php
+++ code/php/action/captcha.php
@@ -25,27 +25,11 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-/*
-Idea original para programar este captcha obtenida de este post:
-- http://sentidoweb.com/2007/01/03/laboratorio-ejemplo-de-captcha.php
-Tambien aparece en otros posts buscando en google:
-- http://www.google.es/search?q=captcha+alto_linea
-*/
 require_once "php/libaction.php";
 $id = getDefault("captcha/id", "captcha");
-$width = getDefault("captcha/width", 90);
-$height = getDefault("captcha/height", 45);
-$letter = getDefault("captcha/letter", 8);
-$number = getDefault("captcha/number", 16);
-$angle = getDefault("captcha/angle", 10);
-$color = getDefault("captcha/color", "5C8ED1");
-$bgcolor = getDefault("captcha/bgcolor", "C8C8C8");
-$fgcolor = getDefault("captcha/fgcolor", "B4B4B4");
 $type = getDefault("captcha/type", "number");
 $length = getDefault("captcha/length", 5);
-$period = getDefault("captcha/period", 2);
-$amplitude = getDefault("captcha/amplitude", 8);
-$blur = getDefault("captcha/blur", "true");
+
 init_random();
 // DEFINE THE CODE AND REAL CAPTCHA
 if ($type == "number") {
@@ -72,110 +56,7 @@ if ($type == "number") {
 } else {
     action_denied();
 }
-// CREATE THE BACKGROUND IMAGE
-$im = imagecreatetruecolor($width, $height);
-$color2 = imagecolorallocate(
-    $im,
-    __captcha_color2dec($color, "R"),
-    __captcha_color2dec($color, "G"),
-    __captcha_color2dec($color, "B")
-);
-$bgcolor2 = imagecolorallocate(
-    $im,
-    __captcha_color2dec($bgcolor, "R"),
-    __captcha_color2dec($bgcolor, "G"),
-    __captcha_color2dec($bgcolor, "B")
-);
-$fgcolor2 = imagecolorallocate(
-    $im,
-    __captcha_color2dec($fgcolor, "R"),
-    __captcha_color2dec($fgcolor, "G"),
-    __captcha_color2dec($fgcolor, "B")
-);
-imagefill($im, 0, 0, $bgcolor2);
-$letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-$font = getcwd() . "/lib/fonts/GorriSans.ttf";
-$bbox = imagettfbbox($letter, 0, $font, $letters[0]);
-$heightline = abs($bbox[7] - $bbox[1]);
-$numlines = intval($height / $heightline) + 1;
-$maxletters = strlen($letters);
-for ($i = 0; $i < $numlines; $i++) {
-    $posx = 0;
-    $posy = ($heightline / 2) + ($heightline + $letter / 4) * $i;
-    while ($posx < $width) {
-        $oneletter = $letters[rand(0, $maxletters - 1)];
-        $oneangle = rand(-$angle, $angle);
-        $bbox = imagettfbbox($letter, $oneangle, $font, $oneletter);
-        imagettftext($im, $letter, rand(-$angle, $angle), (int)$posx, (int)$posy, $fgcolor2, $font, $oneletter);
-        $posx += $bbox[2] - $bbox[0] + $letter / 4;
-    }
-}
-// CREATE THE CAPTCHA CODE
-$im2 = imagecreatetruecolor($width, $height);
-$color2 = imagecolorallocate(
-    $im2,
-    __captcha_color2dec($color, "R"),
-    __captcha_color2dec($color, "G"),
-    __captcha_color2dec($color, "B")
-);
-$bgcolor2 = imagecolorallocate(
-    $im2,
-    __captcha_color2dec($bgcolor, "R"),
-    __captcha_color2dec($bgcolor, "G"),
-    __captcha_color2dec($bgcolor, "B")
-);
-$fgcolor2 = imagecolorallocate(
-    $im2,
-    __captcha_color2dec($fgcolor, "R"),
-    __captcha_color2dec($fgcolor, "G"),
-    __captcha_color2dec($fgcolor, "B")
-);
-imagefill($im2, 0, 0, $bgcolor2);
-imagecolortransparent($im2, $bgcolor2);
-$angles = array();
-$widths = array();
-$heights = array();
-$widthsum = 0;
-for ($i = 0; $i < strlen($code); $i++) {
-    $angles[$i] = rand(-$angle, $angle);
-    $bbox = imagettfbbox($number, $angles[$i], $font, $code[$i]);
-    $widths[$i] = abs($bbox[2] - $bbox[0]);
-    $heights[$i] = abs($bbox[7] - $bbox[1]);
-    $widthsum += $widths[$i];
-}
-$widthmiddle = $width / 2;
-$heightmiddle = $height / 2;
-$posx = $widthmiddle - $widthsum / 2;
-for ($i = 0; $i < strlen($code); $i++) {
-    $posy = $heights[$i] / 2 + $heightmiddle;
-    imagettftext($im2, $number, $angles[$i], (int)$posx, (int)$posy, $color2, $font, $code[$i]);
-    $posx += $widths[$i];
-}
-// COPY THE CODE TO BACKGROUND USING WAVE TRANSFORMATION
-$rel = M_PI / 180;
-$inia = rand(0, 360);
-$inib = rand(0, 360);
-for ($i = 0; $i < $width; $i++) {
-    $a = sin((($i * $period) + $inia) * $rel) * $amplitude;
-    for ($j = 0; $j < $height; $j++) {
-        $b = sin((($j * $period) + $inib) * $rel) * $amplitude;
-        if ($i + $b >= 0 && $i + $b < $width && $j + $a >= 0 && $j + $a < $height) {
-            imagecopymerge($im, $im2, $i, $j, (int)($i + $b), (int)($j + $a), 1, 1, 100);
-        }
-    }
-}
-// APPLY BLUR
-if (eval_bool($blur)) {
-    if (function_exists("imagefilter")) {
-        imagefilter($im, IMG_FILTER_GAUSSIAN_BLUR);
-    }
-}
-// CONTINUE
-ob_start();
-imagepng($im);
-$buffer = ob_get_clean();
-imagedestroy($im);
-imagedestroy($im2);
+$buffer = __captcha_image($code);
 // OUTPUT IMAGE
 output_handler(array(
     "data" => $buffer,
