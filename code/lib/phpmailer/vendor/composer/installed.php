2a148da5f6b9ba7af61c793028c552f6ee881ec0
Josep Sanz <Josep Sanz josep.sanz@saltos.org>
2023-03-06 23:45:31 +0100

    Updated to SaltOS r9968

--- code/lib/phpmailer/vendor/composer/installed.php
+++ code/lib/phpmailer/vendor/composer/installed.php
@@ -20,9 +20,9 @@
             'dev_requirement' => false,
         ),
         'phpmailer/phpmailer' => array(
-            'pretty_version' => 'v6.7.1',
-            'version' => '6.7.1.0',
-            'reference' => '49cd7ea3d2563f028d7811f06864a53b1f15ff55',
+            'pretty_version' => 'v6.8.0',
+            'version' => '6.8.0.0',
+            'reference' => 'df16b615e371d81fb79e506277faea67a1be18f1',
             'type' => 'library',
             'install_path' => __DIR__ . '/../phpmailer/phpmailer',
             'aliases' => array(),
