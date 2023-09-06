10303312ee15cb53bae4151b0bef7336f27ba304
Josep Sanz <Josep Sanz josep.sanz@saltos.org>
2023-09-06 23:45:19 +0200

    Updated to SaltOS r10012

--- code/lib/tcpdf/vendor/composer/installed.php
+++ code/lib/tcpdf/vendor/composer/installed.php
@@ -20,9 +20,9 @@
             'dev_requirement' => false,
         ),
         'tecnickcom/tcpdf' => array(
-            'pretty_version' => '6.6.2',
-            'version' => '6.6.2.0',
-            'reference' => 'e3cffc9bcbc76e89e167e9eb0bbda0cab7518459',
+            'pretty_version' => '6.6.5',
+            'version' => '6.6.5.0',
+            'reference' => '5fce932fcee4371865314ab7f6c0d85423c5c7ce',
             'type' => 'library',
             'install_path' => __DIR__ . '/../tecnickcom/tcpdf',
             'aliases' => array(),
